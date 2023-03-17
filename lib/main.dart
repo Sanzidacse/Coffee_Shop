15fa62cbb605e346006416e1a236f53f82ed7ef2
unknown <sanzidaislam12.cse@gmail.com>
2023-03-17 20:47:40 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:coffee_shop/screen/welcome.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFF212325)),
+      home: Welcome_Screen(),
+    );
+  }
+}
---
+++ lib/screen/welcome.dart
@@ -0,0 +1,82 @@
+import 'package:coffee_shop/screen/homescreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Welcome_Screen extends StatefulWidget {
+  const Welcome_Screen({super.key});
+
+  @override
+  State<Welcome_Screen> createState() => _Welcome_ScreenState();
+}
+
+class _Welcome_ScreenState extends State<Welcome_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        decoration: BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+                image: AssetImage("images/bg.png"),
+                fit: BoxFit.cover,
+                opacity: 0.6)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Coffee Shop",
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Feeling Low? Take a Sip of Coffee",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1,
+                  ),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Material(
+                  color: Color(0xFFE57734),
+                  borderRadius: BorderRadius.circular(15),
+                  child: InkWell(
+                    splashColor: Colors.black,
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen()));
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      child: Text(
+                        "Get Start",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
